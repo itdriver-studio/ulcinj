1e180b74c2b4a8f80c382ce6bb326fff4081caad
Dmitrii Ivanov <itdriver@yahoo.com>
2022-11-01 20:51:25 +0100

    slick

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ burger.onclick = () => {
   menu.classList.toggle('nav__list-adaptive');
 }
 
-let swiper = new Swiper(".swiper", {
-  loop: true,
-  autoplay: {
-    delay: 500,
-  },
-  speed: 1800,
-  effect: 'fade',
+$('.excursions__slider').slick({
+  autoplay: true,
+  speed: 2500,
+  autoplaySpeed: 1500,
+  arrows: false,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  fade: true,
 });
